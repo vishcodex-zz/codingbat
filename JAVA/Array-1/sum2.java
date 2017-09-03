8e455bc0c6084fd54e78d9c3ae8558ac250548d3
git test <vishnu.mits@gmail.com>
2017-09-03 16:40:45 -0400

    added solutions to Array-1

---
+++ JAVA/Array-1/sum2.java
@@ -0,0 +1,4 @@
+/* Given an array of ints, return the sum of the first 2 elements in the array. 
+If the array length is less than 2, just sum up the elements that exist, returning 0 
+if the array is length 0. */
+
